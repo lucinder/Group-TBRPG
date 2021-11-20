ae8623540fc1eb701282972750825af0714346de
aboud <arshaath@uw.edu>
2021-11-20 01:12:05 -0800

    I just added a new Prototype Maven project file, I also added some test files so we can test the code

---
+++ GameProtoType/src/main/java/JunitTests/RPG_DungeonTest.java
@@ -0,0 +1,7 @@
+package main.java.JunitTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RPG_DungeonTest {
+
+}
---
+++ GameProtoType/src/main/java/JunitTests/RPG_RoomTest.java
@@ -0,0 +1,11 @@
+package main.java.JunitTests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RPG_RoomTest {
+    @Test
+    @DisplayName("")
+}
