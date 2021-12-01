586c2831dba9160be1301bf676b9fab93b71c840
Aboud <arshaath@uw.edu>
2021-11-30 19:16:19 -0800

    Deleting Some Unneccairy stuff

--- GrpProj/DEMO_01.java
+++ GrpProj/DEMO_01.java
@@ -1,6 +1,6 @@
 import java.io.*;
 public class DEMO_01{
-   public static void main(String[] args) throws IOException{
+   public static void main(String args[]) throws IOException{
       RPG_Game.start();
    }
 }
--- GrpProj/RPG_Game.java
+++ GrpProj/RPG_Game.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class RPG_Game{
    public static void start() throws IOException{
-      int levels = 9;
-      int levelLength = 4;
+      int levels = 5;
+      int levelLength = 2;
       System.out.println("Dungeon of " + levels + " levels with " + levelLength + " main rooms per level generated:");
       RPG_DungeonBuilder newDungeon = new RPG_DungeonBuilder(levels,levelLength);
       RPG_Player player = RPG_Character_Builder.buildCharacter();
